9b8edbe143190871dda736845acb5869b1d6e0dd
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-12-19 19:07:36 +0600

    review model update

--- src/app/modules/reviews/review.model.ts
+++ src/app/modules/reviews/review.model.ts
@@ -57,7 +57,7 @@ const reviewSchema = new Schema<IReview>(
         values: ['pending', 'published', 'unpublished'],
         message: 'Status must be pending, published, or unpublished',
       },
-      default: 'pending',
+      default: 'published',
     },
     moderationReason: {
       type: String,
